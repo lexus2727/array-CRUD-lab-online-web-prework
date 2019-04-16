7a5d1df75e5bd0cb111606ebdb4cdf44296aac6f
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-16 21:28:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,28 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  kids =["Harry", "Beth", "Jacob", "Tiffany"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array = ["wow", "I", "am", "really", "learning"]
+ element = "arrays!"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ array = ["I", "am", "really", "learning"]
+    element = "wow"
+    array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
